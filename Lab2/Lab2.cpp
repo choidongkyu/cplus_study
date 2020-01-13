a9bb4a1852b813f5ed74de1c717f0e68d1d9b5d9
dongkyu <ehdqkwk@naver.com>
2020-01-13 17:55:19 +0900

    fix

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -4,76 +4,76 @@
 
 namespace lab2
 {
-    std::string Trash;
-    void PrintIntegers(std::istream& in, std::ostream& out)
-    {
-        int num;
-        out << std::setfill(' ') << std::setw(12) << "oct"
-            << std::setw(11) << "dec"
-            << std::setw(9) << "hex" << std::endl;
-        out << std::setfill('-') << std::setw(13) << ' '
-            << std::setw(11) << ' '
-            << std::setw(8) << "" << std::endl;
+	std::string Trash;
+	void PrintIntegers(std::istream& in, std::ostream& out)
+	{
+		int number;
+		out << std::setfill(' ') << std::setw(12) << "oct"
+			<< std::setw(11) << "dec"
+			<< std::setw(9) << "hex" << std::endl;
+		out << std::setfill('-') << std::setw(13) << ' '
+			<< std::setw(11) << ' '
+			<< std::setw(8) << "" << std::endl;
 
-        while (true)
-        {
-            in >> num;
+		while (true)
+		{
+			in >> number;
 
-            if (in.fail())
-            {
-                in.clear();
-                in >> trash;
-            }
-            else
-            {
-                out << std::setfill(' ') << std::setw(12) << std::oct << num
-                    << std::setw(11) << std::dec << num
-                    << std::setw(9) << std::uppercase << std::hex << num << std::endl;
-            }
+			if (in.fail())
+			{
+				in.clear();
+				in >> Trash;
+			}
+			else
+			{
+				out << std::setfill(' ') << std::setw(12) << std::oct << number
+					<< std::setw(11) << std::dec << number
+					<< std::setw(9) << std::uppercase << std::hex << number << std::endl;
+			}
 
-            if (in.eof()) {
-                break;
-            }
-        }
-    }
+			if (in.eof()) {
+				break;
+			}
+		}
+	}
 
-    void PrintMaxFloat(std::istream& in, std::ostream& out)
-    {
-        float num;
-        float max;
-        bool bChk_first = true;
-        while (true)
-        {
-            in >> num;
+	void PrintMaxFloat(std::istream& in, std::ostream& out)
+	{
+		float number;
+		float max;
+		bool bChk_first = true;
+		while (true)
+		{
+			in >> number;
 
-            if (bChk_first)
-            {
-                max = num;
-                bChk_first = false;
-            }
+			if (bChk_first)
+			{
+				max = number;
+				bChk_first = false;
+			}
 
-            if (num > max && !in.fail())
-            {
-                max = num;
-            }
+			if(number > max && !in.fail())
+			{
+				max = number;
+			}
 
-            if (in.fail())
-            {
-                in.clear();
-                in >> trash;
-            }
-            else
-            {
-                out << std::setw(5) << std::setfill(' ') << ""
-                    << std::setw(15) << std::showpos << std::fixed << std::setprecision(3) << std::internal << num << std::endl;
-            }
+			if (in.fail())
+			{
+				in.clear();
+				in >> Trash;
+			}
+			else
+			{
+				out << std::setw(5) << std::setfill(' ') << ""
+					<< std::setw(15) << std::showpos << std::fixed << std::setprecision(3) << std::internal << number << std::endl;
+			}
 
-            if (in.eof())
-            {
-                out << std::setw(5) << std::setfill(' ') << std::left << "max:"
-                    << std::setw(15) << std::showpos << std::fixed << std::setprecision(3) << std::internal << max << std::endl;
-                break;
-            }
-        }
-    }
+			if (in.eof())
+			{
+				out << std::setw(5) << std::setfill(' ') << std::left << "max:"
+					<< std::setw(15) << std::showpos << std::fixed << std::setprecision(3) << std::internal << max << std::endl;
+				break;
+			}
+		}
+	}
 }
