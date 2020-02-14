dcab5cf512c58dad8e5a234c731c5265031fed54
dongkyu <ehdqkwk@naver.com>
2020-02-14 20:13:46 +0900

    fix

--- Lab6/Lab6.cpp
+++ Lab6/Lab6.cpp
@@ -1,26 +1,61 @@
 #include "Lab6.h"
+#include <iostream>
 using namespace std;
 namespace lab6
 {
 	int Sum(const std::vector<int>& v)
 	{
-		
-		return 0;
+		int sum = 0;
+		vector<int>::const_iterator iter = v.begin();
+		for (size_t i = 0; i < v.size(); i++)
+		{
+			sum += *iter;
+			++iter;
+		}
+		return sum;
 	}
 
 	int Min(const std::vector<int>& v)
 	{
-		return 0;
+		int min = INT32_MAX;
+		if (v.size() == 0)
+		{
+			return min;
+		}
+		vector<int>::const_iterator iter = v.begin();
+		for (size_t i = 0; i < v.size(); i++)
+		{
+			if (min > * iter)
+			{
+				min = *iter;
+			}
+			++iter;
+		}
+		return min;
 	}
 
 	int Max(const std::vector<int>& v)
 	{
-		return 0;
+		int max = -INT32_MAX;
+		vector<int>::const_iterator iter = v.begin();
+		if (v.size() == 0)
+		{
+			return max;
+		}
+		for (size_t i = 0; i < v.size(); i++)
+		{
+			if (max < *iter)
+			{
+				max = *iter;
+			}
+			++iter;
+		}
+		return max;
 	}
 
 	float Average(const std::vector<int>& v)
 	{
-		return 0.0f;
+		return static_cast<float>(Sum(v)) / v.size();
 	}
 
 	int NumberWithMaxOccurrence(const std::vector<int>& v)
@@ -30,12 +65,12 @@ namespace lab6
 			return 0;
 		}
 		vector<int>::const_iterator iter = v.begin();
-		int max = -INT16_MAX;
+		int max = -INT32_MAX;
 		size_t maxCount = 0;
 		while (iter != v.end())
 		{
 			size_t count = 0;
-			for (int i = 0; i < v.size(); i++)
+			for (size_t i = 0; i < v.size(); i++)
 			{
 				if (*iter == v[i])
 				{
@@ -54,7 +89,28 @@ namespace lab6
 
 	void SortDescending(std::vector<int>& v)
 	{
+		vector<int>::const_iterator iter = v.begin();
+		int tmp = 0;
+		for (size_t i = 0; i < v.size(); i++)
+		{
+			for (size_t j = 0; j < v.size() - i - 1; j++)
+			{
+				if (*iter < *(iter + 1))
+				{
+					tmp = v[j];
+					v[j] = v[j+1];
+					v[j + 1] = tmp;
+				}
+			}
+		}
+	}
 
+	void Printf(std::vector<int>& v)
+	{
+		for (size_t i = 0; i < v.size(); i++)
+		{
+			std::cout << v[i] << std::endl;
+		}
 	}
 
 }
--- Lab6/main.cpp
+++ Lab6/main.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
 #include "Lab6.h"
 using namespace std;
+using namespace lab6;
 int main()
 {
 	std::vector<int> v;
 	v.push_back(4);
 	v.push_back(3);
-	v.push_back(3);
-	v.push_back(3);
-	v.push_back(11);
-	v.push_back(11);
 	v.push_back(4);
 	v.push_back(12);
 	v.push_back(7);
 	v.push_back(11);
 
-	cout<<lab6::NumberWithMaxOccurrence(v)<<endl; // 4
+	lab6::SortDescending(v); // vÀÇ ¿ä¼Ò: [ 12, 11, 7, 4, 4, 3 ]
+	Printf(v);
 	return 0;
 }
