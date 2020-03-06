5d6408354afedb8df36a0652321865d680b5b2e8
dongkyu <ehdqkwk@naver.com>
2020-03-07 00:20:24 +0900

    ass3 push

---
+++ Assignment3/SmartStack.h
@@ -0,0 +1,146 @@
+#pragma once
+#include <stack>
+#include <limits.h>
+#include <cmath>
+namespace assignment3
+{
+	template <typename T>
+	class SmartStack
+	{
+	public:
+		SmartStack();
+		~SmartStack() = default;
+		void Push(T data);
+		T Pop();
+		T Peek();
+		T GetMax();
+		T GetMin();
+		double GetAverage();
+		T GetSum();
+		double GetVariance();
+		double GetStandardDeviation();
+		unsigned int GetCount();
+
+	private:
+		std::stack<T> mStack;
+		std::stack<T> mMaxStack;
+		std::stack<T> mMinStack;
+		T mSum;
+		T mSquaredSum;
+	};
+
+	template<typename T>
+	inline SmartStack<T>::SmartStack()
+		: mSum(0)
+		, mSquaredSum(0)
+	{
+	}
+
+	template<typename T>
+	inline void SmartStack<T>::Push(T data)
+	{
+		if (mStack.empty())
+		{
+			mMaxStack.push(data);
+			mMinStack.push(data);
+		}
+		else
+		{
+			if (data > mMaxStack.top())
+			{
+				mMaxStack.push(data);
+			}
+			else {
+				mMaxStack.push(mMaxStack.top());
+			}
+
+			if (data < mMinStack.top())
+			{
+				mMinStack.push(data);
+			}
+			else
+			{
+				mMinStack.push(mMinStack.top());
+			}
+		}
+		mStack.push(data);
+		mSum += data;
+		mSquaredSum += data * data;
+
+	}
+
+	template<typename T>
+	inline T SmartStack<T>::Pop()
+	{
+		T value = mStack.top();
+		mSum -= mStack.top();
+		mSquaredSum -= mStack.top() * mStack.top();
+		mStack.pop();
+		mMaxStack.pop();
+		mMinStack.pop();
+
+		return value;
+	}
+
+	template<typename T>
+	inline T SmartStack<T>::Peek()
+	{
+		return mStack.top();
+	}
+
+	template<typename T>
+	inline T SmartStack<T>::GetMax()
+	{
+		if (mStack.empty())
+		{
+			return std::numeric_limits<T>::min();
+		}
+		return mMaxStack.top();
+	}
+
+	template<typename T>
+	inline T SmartStack<T>::GetMin()
+	{
+		if (mStack.empty())
+		{
+			return std::numeric_limits<T>::max();
+		}
+		return mMinStack.top();
+	}
+
+	template<typename T>
+	inline double SmartStack<T>::GetAverage()
+	{
+		double ave = static_cast <double>(mSum) / mStack.size();
+		
+		return round(ave * 1000) / 1000;
+	}
+
+	template<typename T>
+	inline T SmartStack<T>::GetSum()
+	{
+		return mSum;
+	}
+
+	template<typename T>
+	inline double SmartStack<T>::GetVariance()
+	{
+		double ave = GetAverage();
+		double deviation = (static_cast<double>(mSquaredSum) / mStack.size()) - (ave * ave);
+		return round(deviation * 1000) / 1000;
+	}
+
+	template<typename T>
+	inline double SmartStack<T>::GetStandardDeviation()
+	{
+		double result = sqrt(GetVariance());
+		return round(result * 1000) / 1000;
+	}
+		
+
+	template<typename T>
+	inline unsigned int SmartStack<T>::GetCount()
+	{
+		return mStack.size();
+	}
+}
--- Assignment3/main.cpp
+++ Assignment3/main.cpp
@@ -1,4 +1,18 @@
+#include <iostream>
+#include "SmartStack.h"
+
+using namespace assignment3;
+using namespace std;
 int main()
 {
+	SmartStack<int> s;
+	s.Push(6);
+	s.Push(5);
+	s.Push(-2); // [ 6, 5, -2 ]
 
+	double standardDeviation = s.GetStandardDeviation(); // 3.559
+
+	//int popped = s.Peek(); // popped의 값은 -2. s의 요소: [ 6, 5 ]
+	cout << standardDeviation << endl;
+	return 0;
 }
