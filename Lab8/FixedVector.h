348e550b042787a79542525dbc0ba283fcb96569
dongkyu <ehdqkwk@naver.com>
2020-03-12 18:57:45 +0900

    first push lab8

---
+++ Lab8/FixedVector.h
@@ -0,0 +1,98 @@
+#pragma once
+#include <iostream>
+namespace lab8
+{
+	template<typename T, size_t N>
+	class FixedVector
+	{
+	public:
+		FixedVector();
+		~FixedVector();
+		bool Add(const T data);
+		bool Remove(const T data);
+		int GetIndex(const T data) const;
+		void Printf() const;
+		T Get(const unsigned int index) const;
+		T operator[](const unsigned int index) const;
+	private:
+		size_t mSize;
+		const T* mArray[N];
+	};
+
+	template<typename T, size_t N>
+	inline FixedVector<T, N>::FixedVector()
+		: mSize(0)
+		, mArray()
+	{
+	}
+
+	template<typename T, size_t N>
+	inline bool FixedVector<T, N>::Add(const T data)
+	{
+		if (mSize >= N)
+		{
+			return false;
+		}
+		T* var = new T();
+		*var = data;
+		mArray[mSize] = var;
+		++mSize;
+		return true;
+	}
+
+	template<typename T, size_t N>
+	inline FixedVector<T, N>::~FixedVector()
+	{
+		for (size_t i = 0; i < mSize; ++i)
+		{
+			delete mArray[i];
+		}
+	}
+
+	template<typename T, size_t N>
+	inline bool FixedVector<T, N>::Remove(const T data)
+	{
+		for (size_t i = 0; i < mSize; ++i)
+		{
+			if (*mArray[i] == data)
+			{
+				for (size_t j = i; j < mSize; ++j)
+				{
+					mArray[j] = mArray[j + 1];
+				}
+				--mSize;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	template<typename T, size_t N>
+	inline int FixedVector<T, N>::GetIndex(const T data) const
+	{
+		
+	}
+
+
+	template<typename T, size_t N>
+	inline void FixedVector<T, N>::Printf() const
+	{
+		for (size_t i = 0; i < mSize; ++i)
+		{
+			std::cout << *mArray[i] << std::endl;
+		}
+	}
+	
+	template<typename T, size_t N>
+	inline T FixedVector<T, N>::Get(unsigned int index) const
+	{
+		return *mArray[index];
+	}
+	
+	template<typename T, size_t N>
+	inline T FixedVector<T, N>::operator[](const unsigned int index) const
+	{
+		return *mArray[index];
+	}
+}
+
--- Lab8/main.cpp
+++ Lab8/main.cpp
@@ -1,4 +1,17 @@
+#include "FixedVector.h"
+#include <iostream>
+using namespace std;
+using namespace lab8;
 int main()
 {
+	FixedVector<int, 4> v1;
+	v1.Add(1); // [ 1 ]
+	v1.Add(2); // [ 1, 2 ]
+	v1.Add(3); // [ 1, 2, 3 ]
+	v1.Add(4); // [ 1, 2, 3, 4 ]
 
+	v1.Remove(3); // [ 1, 2, 4 ]
+	v1.Remove(3); // false ¹ÝÈ¯
+
+	return 0;
 }
