102bad6c75f10a33da74bfa5ba4359244d6f4dc8
dkchoi <ehdqkwk@naver.com>
2020-03-16 00:55:27 +0900

    fix

--- Lab8/main.cpp
+++ Lab8/main.cpp
@@ -6,12 +6,13 @@
 using namespace lab8;
 int main()
 {
-	FixedVector<int, 5> v1;
-	v1.Add(1);
-	v1.Add(2);
+	FixedVector<int, 32> v1;
+	/*for (size_t i = 0; i < 31; ++i)
+	{
+		v1.Add(false);
+	}*/
 	v1.Add(3);
-	v1.Add(4);
 
-	v1.Get(2)=3; // 4 반환
+	std::cout<<v1.Get(0)<<std::endl; // 1 반환
 	return 0;
 }
